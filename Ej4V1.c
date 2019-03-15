41ab170b2dba4e504943fe962b48552629bad889
MartinRodriguez1209 <martinrodriguez.1209@gmail.com>
2019-03-15 08:22:00 -0300

    as

--- Ej4V1.c
+++ Ej4V1.c
@@ -9,7 +9,6 @@ int  main() {
 	if (pid == 0) {
 		printf("hijo Pid:%d \n",getpid());
 	} else {
-
     waitpid(-1,&status,0);
     printf("padre Pid:%d \n" ,getpid());
     printf("Estado de salida del hijo=%d  \n",status );
